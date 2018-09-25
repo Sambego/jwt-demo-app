de82f1b337e8a1abcc08c06176c9fe62091f467a
Sambego <sambellen@gmail.com>
2018-09-25 11:12:47 +0200

    Remove unused Login import

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import Nav from "./Nav/Nav";
 import Container from "./Container/Container";
-import Login from "./Login/Login";
 import Picture from "./Picture/Picture";
 import Callback from "./Callback/Callback";
 
-export { Nav, Container, Login, Picture, Callback };
+export { Nav, Container, Picture, Callback };
